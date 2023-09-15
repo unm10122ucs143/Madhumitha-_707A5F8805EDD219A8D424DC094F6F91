a03581da3215011632c5d7b1bcb4617ba6796fb1
unm10122ucs143 <144410994+unm10122ucs143@users.noreply.github.com>
2023-09-15 12:53:30 +0530

    Add files via upload

--- main (1).py
+++ main (1).py
@@ -1,15 +1,19 @@
-# Default function to implement conditions to check leap year  
-def CheckLeap(Year):  
-  # Checking if the given year is leap year  
-  if((Year % 400 == 0) or  
-     (Year % 100 != 0) and  
-     (Year % 4 == 0)):   
-    print("Given Year is a leap Year");  
-  # Else it is not a leap year  
-  else:  
-    print ("Given Year is not a leap Year")  
-# Taking an input year from user  
-Year = int(input("Enter the number: "))  
-# Printing result  
-CheckLeap(Year)  
+class Player:
+    def play(self):
+        print("The player is playing cricket.")
 
+class Batsman(Player):
+    def play(self):
+        print("The batsman is batting.")
+
+class Bowler(Player):
+    def play(self):
+        print("The bowler is bowling.")
+
+# Create objects of Batsman and Bowler classes
+batsman = Batsman()
+bowler = Bowler()
+
+# Call the play() method for each object
+batsman.play()
+bowler.play()
--- main.py
+++ main.py
@@ -1,17 +1,46 @@
+class BankAccount:
+    def __init__(self, account_number, account_holder_name, initial_balance=0.0):
+        self.__account_number = account_number
+        self.__account_holder_name = account_holder_name
+        self.__account_balance = initial_balance
 
-# Recursive function to find factorial of a number
-def factorial(n):
- 
-    # base case: if `n` is 0 or 1
-    if n < 1:
-        return 1
- 
-    # use the recurrence relation
-    return n * factorial(n - 1)
- 
- 
-if __name__ == '__main__':
- 
-    n = 5
-    print(f'The Factorial of {n} is', factorial(n))
- 
+    def deposit(self, amount):
+        if amount > 0:
+            self.__account_balance += amount
+            print(f"Deposited ${amount}. New balance: ${self.__account_balance}")
+        else:
+            print("Invalid deposit amount. Amount must be greater than 0.")
+
+    def withdraw(self, amount):
+        if amount > 0:
+            if amount <= self.__account_balance:
+                self.__account_balance -= amount
+                print(f"Withdrew ${amount}. New balance: ${self.__account_balance}")
+            else:
+                print("Insufficient funds.")
+        else:
+            print("Invalid withdrawal amount. Amount must be greater than 0.")
+
+    def display_balance(self):
+        print(f"Account balance for {self.__account_holder_name} (Account #{self.__account_number}): ${self.__account_balance}")
+
+
+# Testing the BankAccount class
+if __name__ == "__main__":
+    # Create a bank account
+    account = BankAccount("12345", "John Doe", 1000.0)
+
+    # Display initial balance
+    account.display_balance()
+
+    # Make a deposit
+    account.deposit(500.0)
+
+    # Make a withdrawal
+    account.withdraw(200.0)
+
+    # Try to withdraw more than the balance
+    account.withdraw(1500.0)
+
+    # Display the updated balance
+    account.display_balance()
